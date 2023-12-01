52d8fa644d3c5f08c5795679576a028663f1ee3b
Mira Rzf <mira.razaf@gmail.com>
2023-12-01 13:12:06 +0100

    Day 01 in python

---
+++ pysolutions/day01/day01.py
@@ -0,0 +1,32 @@
+def solve(inputfile, puzzlepart): 
+    f = open(inputfile, 'r')
+    lines = f.readlines()
+
+    digits = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven':7, 'eight':8, 'nine':9}
+    digits=['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    sumFirstDigits = 0 
+    sumLastDigits = 0 
+    lastDigit = 0
+    for line in lines: 
+        first = True 
+        for i, e in enumerate(line): 
+            if ord(e) > 47 and ord(e) < 58: 
+                if first: 
+                    firstDigit = int(e)
+                    first = False 
+                lastDigit = int(e)
+        
+            if puzzlepart == 2: 
+                for index, digit in enumerate(digits): 
+                    if line[i:i+len(digit)] == digit: 
+                        if first: 
+                            firstDigit = index+1 
+                            first = False 
+                        lastDigit = index+1 
+
+        sumFirstDigits += firstDigit
+        sumLastDigits += lastDigit
+        print(line, firstDigit, lastDigit)
+    
+    return sumFirstDigits*10+sumLastDigits
