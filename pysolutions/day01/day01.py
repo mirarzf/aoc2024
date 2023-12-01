85d66b6f6c05f9c31f92482f0e9bf68bc515aea5
Mira Rzf <mira.razaf@gmail.com>
2023-12-01 14:08:19 +0100

    Erased useless dictionary.

--- pysolutions/day01/day01.py
+++ pysolutions/day01/day01.py
@@ -2,7 +2,6 @@ def solve(inputfile, puzzlepart):
     f = open(inputfile, 'r')
     lines = f.readlines()
 
-    digits = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven':7, 'eight':8, 'nine':9}
     digits=['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
     sumDigits = 0 
