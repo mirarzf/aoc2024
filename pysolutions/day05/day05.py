9775208d20ebff665c89d930a002e97365714766
Mira Rzf <mira.razaf@gmail.com>
2023-12-06 13:38:23 +0100

    Updated day05 python part 1 solution

--- pysolutions/day05/day05.py
+++ pysolutions/day05/day05.py
@@ -1,3 +1,19 @@
+def mapModifier(newRules, currentMapping): 
+    newMapping = {}
+    for rule in newRules: 
+        dest, source, rangeRule = rule[0], rule[1], rule[2]
+        for key in currentMapping.keys(): 
+            old_dest = currentMapping[key]
+            if old_dest >= source and old_dest < source+rangeRule: 
+                newvalue = dest + old_dest - source
+                newMapping[key] = newvalue
+    
+    for key in currentMapping.keys(): 
+        if key not in newMapping.keys(): 
+            newMapping[key] = currentMapping[key]
+    
+    return newMapping
+
 def solve(inputfile : str, puzzlepart : int): 
     f = open(inputfile, 'r')
     lines = f.readlines()
@@ -15,17 +31,20 @@ def solve(inputfile : str, puzzlepart : int):
         if line[0].isnumeric(): 
             smallMap = [int(e) for e in line.split()]
             mapsList[-1].append(smallMap)
-    
-    newseeds = []
-    
-    for seed in seeds: 
-        seedvalue = seed 
-        for carte in mapsList: 
-            i = 0
-            while i < len(carte) and (seedvalue < carte[i][1] or seedvalue > carte[i][1]+carte[i][2]): 
-                i += 1
-            if i < len(carte): 
-                seedvalue = carte[i][0]+(seedvalue-carte[i][1]) 
-        newseeds.append(seedvalue)
 
-    return min(newseeds) 
+    mapping = {}
+    if puzzlepart == 1: 
+        for seed in seeds: 
+            mapping[seed] = seed
+
+    if puzzlepart == 2: 
+        n = len(seeds)//2 
+        for i in range(n): 
+            seedSource = seeds[2*i]
+            for j in range(seeds[2*i+1]): 
+                mapping[seedSource+j] = seedSource+j
+
+    for carte in mapsList: 
+        mapping = mapModifier(carte, mapping)
+    
+    return min(mapping.values())
