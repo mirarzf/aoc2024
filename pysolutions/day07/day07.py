41125dafc3699ea566199a46de8f3b22c65d7eea
mirar <mira.razaf@gmail.com>
2024-12-08 17:19:35 +0100

    Temporary solution that does not work for day 7 p 1

--- pysolutions/day07/day07.py
+++ pysolutions/day07/day07.py
@@ -1,9 +1,30 @@
-import re 
+def formatLine(line): 
+    newLine = line.split(' ')
+    newLine[0] = newLine[0][:-1]
+    return [int(f) for f in newLine]
+
+def isValid(line): 
+    if len(line) == 2: 
+        if line[0]==line[1]: 
+            return True 
+        else: 
+            return False 
+
+    else: 
+        newLinePlus = [line[0]-line[-1]] + line[1:-1]
+        newLinePlusValidity = isValid(newLinePlus)
+        newLineMul = [line[0]//line[-1]] + line[1:-1]
+        newLineMulValidity = isValid(newLineMul)
+        return newLinePlusValidity or newLineMulValidity
 
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
-    lines = [line.rstrip('\n') for line in f.readlines()] 
-    f.close()
-    nrows, ncols = len(lines), len(lines[0])
 
-    return 0
+    sommeOKlines = 0
+    for line in f.readlines(): 
+        correctLine = formatLine(line.rstrip('\n'))
+        if isValid(correctLine): 
+            sommeOKlines += correctLine[0]
+
+    f.close()
+    return sommeOKlines
