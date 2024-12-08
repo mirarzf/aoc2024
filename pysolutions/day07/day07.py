104a721800ddb4ab882ab3b00b87ff1ed8efbb7f
mirar <mira.razaf@gmail.com>
2024-12-08 18:58:45 +0100

    Day 7 python solution

--- pysolutions/day07/day07.py
+++ pysolutions/day07/day07.py
@@ -3,7 +3,11 @@ def formatLine(line):
     newLine[0] = newLine[0][:-1]
     return [int(f) for f in newLine]
 
-def isValid(line): 
+def unconcatenateInts(T1, T2): 
+    tenPow = len(str(T2))
+    return T1//(10**tenPow)
+
+def isValid(line, puzzlepart = 1): 
     if len(line) == 2: 
         if line[0]==line[1]: 
             return True 
@@ -11,20 +15,36 @@ def isValid(line):
             return False 
 
     else: 
+        condPuzzle1 = line[0]<line[1] and line[0]%line[1]!=0
+        if puzzlepart == 1 and condPuzzle1: 
+            return False 
+        condPuzzle2 = condPuzzle1 and line[0]%(10**len(str(line[-1])))!=line[-1]
+        if puzzlepart == 2 and condPuzzle2: 
+            return False 
         newLinePlus = [line[0]-line[-1]] + line[1:-1]
-        newLinePlusValidity = isValid(newLinePlus)
-        newLineMul = [line[0]//line[-1]] + line[1:-1]
-        newLineMulValidity = isValid(newLineMul)
-        return newLinePlusValidity or newLineMulValidity
+        newLinePlusValidity = isValid(newLinePlus, puzzlepart)
+        newLineMul = [line[0]/line[-1]] + line[1:-1]
+        newLineMulValidity = isValid(newLineMul, puzzlepart)
+        if puzzlepart == 1: 
+            return newLinePlusValidity or newLineMulValidity
+        else: 
+            if line[0]%(10**len(str(line[-1])))==line[-1]: 
+                newLineConcat = [unconcatenateInts(line[0],line[-1])] + line[1:-1]
+                newLineConcatValidity = isValid(newLineConcat, puzzlepart=puzzlepart)
+            else: 
+                newLineConcatValidity = False 
+            return newLinePlusValidity or newLineMulValidity or newLineConcatValidity
 
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
+    lines = [formatLine(line.rstrip('\n')) for line in f.readlines()]
+    f.close()
 
     sommeOKlines = 0
-    for line in f.readlines(): 
-        correctLine = formatLine(line.rstrip('\n'))
-        if isValid(correctLine): 
-            sommeOKlines += correctLine[0]
+    for line in lines: 
+        if isValid(line, puzzlepart): 
+            sommeOKlines += line[0]
 
-    f.close()
     return sommeOKlines
+
+# 2654749936343 correct 
