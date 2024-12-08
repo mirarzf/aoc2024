332e5823f0a35208134e7832c7f8f6dadbd92382
mirar <mira.razaf@gmail.com>
2024-12-08 19:25:53 +0100

    day 8 part 1 solution

--- pysolutions/day08/day08.py
+++ pysolutions/day08/day08.py
@@ -1,8 +1,21 @@
+def isValidPos(pos, nrows, ncols): 
+    i, j = pos 
+    return i >= 0 and i < nrows and j >= 0 and j < ncols
+
+def getAntinodePosition(pos1, pos2): 
+    if pos1 == pos2: 
+        return -1, -1 
+    
+    deltaI = pos2[0]-pos1[0]
+    deltaJ = pos2[1]-pos1[1]
+
+    return pos1[0]-deltaI, pos1[1]-deltaJ
+
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
     lines = [line.rstrip('\n') for line in f.readlines()]
     f.close()
-    nrow, ncols = len(lines), len(lines[0])
+    nrows, ncols = len(lines), len(lines[0])
 
     # Create dictionary of all antennas types and their positions 
     antennas = {}
@@ -15,10 +28,13 @@ def solve(inputfile, puzzlepart):
                 else: 
                     antennas[antennaType] = [(i,j)]
     
-    print(antennas)
+    # Create antinodes 
+    antinodesPositions = []
+    for antennaType in antennas.keys(): 
+        for antenna1 in antennas[antennaType]: 
+            for antenna2 in antennas[antennaType]: 
+                antinodePos = getAntinodePosition(antenna1, antenna2)
+                if isValidPos(antinodePos, nrows, ncols): 
+                    antinodesPositions.append(antinodePos)
 
-    somme = 0
-    for line in lines: 
-        somme += 1
-
-    return somme
+    return len(set(antinodesPositions))
