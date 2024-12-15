89acf6a0f217fc3ab42a22241d9f1fd576aa8070
mirar <mira.razaf@gmail.com>
2024-12-15 18:30:30 +0100

    Day 15 part 1

--- pysolutions/day15/day15.py
+++ pysolutions/day15/day15.py
@@ -1,5 +1,58 @@
 import numpy as np 
 
+def moveTop(i, j, grid): 
+    posRobot = (i, j)
+    k = i-1
+    while k > 0 and grid[k, j] == 1: 
+        k -= 1
+    if k != 0 and grid[k, j] == 0: 
+        grid[k, j] = 1
+        grid[i-1, j] = 0
+        posRobot = (i-1, j)
+    return posRobot, grid 
+
+def moveBottom(i, j, grid): 
+    posRobot = (i, j)
+    k = i+1
+    while k < grid.shape[0]-1 and grid[k, j] == 1: 
+        k += 1
+    if grid[k, j] == 0: 
+        grid[k, j] = 1
+        grid[i+1, j] = 0
+        posRobot = (i+1, j)
+    return posRobot, grid 
+
+def moveLeft(i, j, grid): 
+    posRobot = (i, j)
+    k = j-1
+    while k > 0 and grid[i, k] == 1: 
+        k -= 1
+    if grid[i, k] == 0: 
+        grid[i, k] = 1
+        grid[i, j-1] = 0
+        posRobot = (i, j-1)
+    return posRobot, grid 
+
+def moveRight(i, j, grid): 
+    posRobot = (i, j)
+    k = j+1
+    while k < grid.shape[1]-1 and grid[i, k] == 1: 
+        k += 1
+    if grid[i, k] == 0: 
+        grid[i, k] = 1
+        grid[i, j+1] = 0
+        posRobot = (i, j+1)
+    return posRobot, grid 
+
+def sumGPScoordinates(grid): 
+    somme = 0 
+    for i in range(grid.shape[0]): 
+        for j in range(grid.shape[1]): 
+            if grid[i,j] == 1: 
+                somme += 100*i + j
+    return somme 
+
+
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
     lines = [line.rstrip('\n') for line in f.readlines()]
@@ -23,21 +76,17 @@ def solve(inputfile, puzzlepart):
         i += 1
     
     posGrid = np.array(posGrid)
-    
-    instructions = []
-    for line in lines[i+1:]: 
-        instrLine = []
-        for j in range(len(line)): 
-            if line[j] == '^': 
-                instrLine.append(0)
-            elif line[j] == '>': 
-                instrLine.append(1)
-            elif line[j] == 'v': 
-                instrLine.append(2)
-            elif line[j] == '<': 
-                instrLine.append(3)
-        instructions.append(instrLine)
-    
-    print(startPos)
 
-    return 0
+    posRobot = startPos
+    for line in lines[i+1:]: 
+        for c in line: 
+            if c == '^': 
+                posRobot, posGrid = moveTop(*posRobot, posGrid)
+            elif c == '>': 
+                posRobot, posGrid = moveRight(*posRobot, posGrid)
+            elif c == 'v': 
+                posRobot, posGrid = moveBottom(*posRobot, posGrid)
+            elif c == '<': 
+                posRobot, posGrid = moveLeft(*posRobot, posGrid)
+
+    return sumGPScoordinates(posGrid)
