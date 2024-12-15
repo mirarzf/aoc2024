f9ba59d965c777d3b00b6e384d45a1645c1f0f9a
mirar <mira.razaf@gmail.com>
2024-12-15 09:38:23 +0100

    Initialized input for day 15

--- pysolutions/day15/day15.py
+++ pysolutions/day15/day15.py
@@ -5,4 +5,33 @@ def solve(inputfile, puzzlepart):
     lines = [line.rstrip('\n') for line in f.readlines()]
     f.close()
     
+    posGrid = []
+
+    i = 0 
+    while lines[i] != '': 
+        ncols = len(lines[i])
+        line = np.zeros(ncols, dtype=int)
+        for j in range(ncols): 
+            if lines[i][j] == 'O': 
+                line[j] = 1 
+            elif lines[i][j] == '#': 
+                line[j] = 2
+        posGrid.append(line)
+        i += 1
+    
+    posGrid = np.array(posGrid)
+    
+    instructions = []
+    for line in lines[i+1:]: 
+        instrLine = []
+        for j in range(len(line)): 
+            if line[j] == '^': 
+                instrLine.append(0)
+            elif line[j] == '>': 
+                instrLine.append(1)
+            elif line[j] == 'v': 
+                instrLine.append(2)
+            elif line[j] == '<': 
+                instrLine.append(3)
+        instructions.append(instrLine)
     return 0
