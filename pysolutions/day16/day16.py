ad2ec8ed7f52d3f3adc368b390559b9894b1d9a3
mirar <mira.razaf@gmail.com>
2024-12-16 22:54:20 +0100

    Updated day 16 part 1 python solution

--- pysolutions/day16/day16.py
+++ pysolutions/day16/day16.py
@@ -2,72 +2,91 @@ import numpy as np
 
 def getNeighbours(pos, grid, distances, directions, preds): 
     i, j = pos 
-    currDist = distances[i,j]
-    currDir = directions[i][j]
+    currDistV = distances['V'][i,j]
+    currDistH = distances['H'][i,j]
     nrows, ncols = len(grid), len(grid[0])
     neighbours = []
+
     # GO UP 
     if i > 0 and grid[i-1][j] != '#': 
-        dist = distances[i-1,j]
-        if currDir == 'H': 
-            tempDist = currDist+1000+1
-        else: 
-            tempDist = currDist+1
+        row, col = i-1, j
+        dist = distances['V'][row,col]
+
+        tempDistV = currDistV+1
+        tempDistH = currDistH+1000+1
+        tempDist = min(tempDistV, tempDistH)
         if dist > tempDist: 
-            distances[i-1,j] = tempDist
-            directions[i-1][j] = 'V'
-            neighbours.append((i-1,j))
-            preds[i-1][j] = (i,j)
-    
+            distances['V'][row,col] = tempDist
+            neighbours.append((row,col))
+            preds['V'][row][col] = (i,j)
+        
+        directions[row][col] = 'V' if distances['H'][row,col] > distances['V'][row,col] else 'H'
+        preds['minimum'][row][col] = preds['V'][row][col] if distances['H'][row,col] > distances['V'][row,col] else preds['H'][row][col]
+
     # GO DOWN 
-    if i < nrows-1 and grid[i+1][j] != '#':  
-        dist = distances[i+1,j]
-        if currDir == 'H': 
-            tempDist = currDist+1000+1
-        else: 
-            tempDist = currDist+1
+    if i < nrows-1 and grid[i+1][j] != '#': 
+        row, col = i+1, j
+        dist = distances['V'][row,col]
+
+        tempDistV = currDistV+1
+        tempDistH = currDistH+1000+1
+        tempDist = min(tempDistV, tempDistH)
         if dist > tempDist: 
-            distances[i+1,j] = tempDist
-            directions[i+1][j] = 'V'
-            neighbours.append((i+1,j))
-            preds[i+1][j] = (i,j)
+            distances['V'][row,col] = tempDist
+            neighbours.append((row,col))
+            preds['V'][row][col] = (i,j)
+        
+        directions[row][col] = 'V' if distances['H'][row,col] > distances['V'][row,col] else 'H'
+        preds['minimum'][row][col] = preds['V'][row][col] if distances['H'][row,col] > distances['V'][row,col] else preds['H'][row][col]
 
     # GO LEFT 
-    if j > 0 and grid[i][j-1] != '#':  
-        dist = distances[i,j-1]
-        if currDir == 'V': 
-            tempDist = currDist+1000+1
-        else: 
-            tempDist = currDist+1
+    if j > 0 and grid[i][j-1] != '#': 
+        row, col = i, j-1
+        dist = distances['H'][row,col]
+
+        tempDistV = currDistV+1000+1
+        tempDistH = currDistH+1
+        tempDist = min(tempDistV, tempDistH)
         if dist > tempDist: 
-            distances[i,j-1] = tempDist
-            directions[i][j-1] = 'H'
-            neighbours.append((i,j-1))
-            preds[i][j-1] = (i,j)
+            distances['H'][row,col] = tempDist
+            neighbours.append((row,col))
+            preds['H'][row][col] = (i,j)
+        
+        directions[row][col] = 'V' if distances['H'][row,col] > distances['V'][row,col] else 'H'
+        preds['minimum'][row][col] = preds['V'][row][col] if distances['H'][row,col] > distances['V'][row,col] else preds['H'][row][col]
 
     # GO RIGHT 
     if j < ncols-1 and grid[i][j+1] != '#': 
-        dist = distances[i,j+1]
-        if currDir == 'V': 
-            tempDist = currDist+1000+1
-        else: 
-            tempDist = currDist+1
+        row, col = i, j+1
+        dist = distances['H'][row,col]
+
+        tempDistV = currDistV+1000+1
+        tempDistH = currDistH+1
+        tempDist = min(tempDistV, tempDistH)
         if dist > tempDist: 
-            distances[i,j+1] = tempDist
-            directions[i][j+1] = 'H'
-            neighbours.append((i,j+1))
-            preds[i][j+1] = (i,j)
+            distances['H'][row,col] = tempDist
+            neighbours.append((row,col))
+            preds['H'][row][col] = (i,j)
+        
+        directions[row][col] = 'V' if distances['H'][row,col] > distances['V'][row,col] else 'H'
+        preds['minimum'][row][col] = preds['V'][row][col] if distances['H'][row,col] > distances['V'][row,col] else preds['H'][row][col]
     
     return neighbours
 
 def Dijkstra(startPos, grid): 
     nrows, ncols = len(grid), len(grid[0])
     
-    distances = np.ones((nrows, ncols), dtype=int)*1000000
+    distances = {}
+    distances['H'] = np.ones((nrows, ncols), dtype = int)*1000000
+    distances['V'] = np.ones((nrows, ncols), dtype = int)*1000000
     directions = [['.' for k1 in range(ncols)] for k2 in range(nrows)]
-    distances[startPos] = 0
+    distances['H'][startPos] = 0
+    distances['V'][startPos] = 1000
     directions[startPos[0]][startPos[1]] = 'H'
-    preds = [[(0,0) for k1 in range(ncols)] for k2 in range(nrows)]
+    preds = {}
+    preds['V'] = [[(0,0) for k1 in range(ncols)] for k2 in range(nrows)]
+    preds['H'] = [[(0,0) for k1 in range(ncols)] for k2 in range(nrows)]
+    preds['minimum'] = [[(0,0) for k1 in range(ncols)] for k2 in range(nrows)]
 
     neighbours = [startPos]
     while len(neighbours) > 0: 
@@ -94,13 +113,17 @@ def solve(inputfile, puzzlepart):
     
     distances, predecessors = Dijkstra(startPos, lines)
     
-    countPreds = 0
-    i, j = endPos
-    while (i,j) != startPos: 
-        countPreds += 1
-        i,j = predecessors[i][j]
-    print(countPreds)
+    # countPreds = 0
+    # i, j = endPos
+    # while (i,j) != startPos: 
+    #     countPreds += 1
+    #     i,j = predecessors['minimum'][i][j]
+    #     print(i,j)
+    # print(countPreds)
 
-    return distances[endPos[0], endPos[1]]
+    return min(distances['H'][endPos], distances['V'][endPos])
 
-#  85440 too high 
+# 85440 too high 
+# 85355 too low 
+
+# Right test answer is 5024 
